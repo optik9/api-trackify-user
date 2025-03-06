d292b9125ffded38073e403163ea8658fcb252e0
optik9 <choyoss@gmail.com>
2025-03-06 14:51:35 -0500

    version 14 - change query allsuers

--- src/models/timeSheetModel.js
+++ src/models/timeSheetModel.js
@@ -34,12 +34,8 @@ const timeSheetModel = {
         users.id AS id_user,
         users.name AS user_name,
         users.status AS status,
-        projects.name AS project_name, 
-        ts.description,
-        ts.time_from,
-        ts.time_to,
-        ts.duration,
-        ts.createdAt,
+        
+    
         CASE
           WHEN TRIM(users.phone) REGEXP '^(\\\\+51|0051)' THEN 'Peru'
           WHEN TRIM(users.phone) REGEXP '^(\\\\+59|0059)' THEN 'Peru'
@@ -48,10 +44,11 @@ const timeSheetModel = {
           WHEN TRIM(users.phone) REGEXP '^(\\\\+63|0063)' THEN 'USA'
           ELSE 'Other'
         END AS location
-      FROM time_sheets ts
-      JOIN users ON ts.user_id = users.id
-      JOIN projects ON ts.project_id = projects.id
+      FROM users
+     
     `;
+
+   
 
     const queryParams = [];
     const whereClauses = [];
@@ -62,11 +59,17 @@ const timeSheetModel = {
       queryParams.push(filters.status);
     }
 
+      // Filtro por location
+      if (filters.location) {
+        whereClauses.push('location = ?');
+        queryParams.push(filters.location);
+      }
+
     // Filtro por fecha
-    if (filters.startDate && filters.endDate) {
-      whereClauses.push('DATE(ts.createdAt) BETWEEN ? AND ?');
-      queryParams.push(filters.startDate, filters.endDate);
-    }
+   // if (filters.startDate && filters.endDate) {
+   //   whereClauses.push('DATE(ts.createdAt) BETWEEN ? AND ?');
+   //   queryParams.push(filters.startDate, filters.endDate);
+   // }
 
     // Construir query final
     if (whereClauses.length > 0) {
@@ -74,7 +77,7 @@ const timeSheetModel = {
     }
 
     // Ordenamiento
-    baseQuery += ' ORDER BY ts.createdAt DESC';
+    //baseQuery += ' ORDER BY ts.createdAt DESC';
 
     // Ejecutar consulta
     const [rows] = await pool.query(baseQuery, queryParams);
